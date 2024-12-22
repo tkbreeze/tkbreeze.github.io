f626c439b9b01fbdd927b6ec52ba3c92a53a7688
tkbreeze <tadoufudao@gmail.com>
2024-12-22 21:29:03 +0900

    文章修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => {
         <h1 className="text-4xl font-bold">Publications</h1>
         <ul className="list-disc pl-5 mt-1">
           <li>
-              Takuto Fukushima, and Ryusuke Miyamoto, "Spatiotemporal Pooling on Appropriate Topological Maps Represented as Two-Dimensional Images for EEG Classification.", Asian Conference on Computer Vision (ACCV), 2024, DOI:<a href="https://doi.org/10.1007/978-981-96-0901-7_4" className="text-blue-600 visited:text-purple-600">https://doi.org/10.1007/978-981-96-0901-7_4</a> (<span className="font-bold">acceptance rate: 32%</span>)
+              Takuto Fukushima, and Ryusuke Miyamoto, "Spatiotemporal Pooling on Appropriate Topological Maps Represented as Two-Dimensional Images for EEG Classification.", Asian Conference on Computer Vision (ACCV) poster, 2024, DOI:<a href="https://doi.org/10.1007/978-981-96-0901-7_4" className="text-blue-600 visited:text-purple-600">https://doi.org/10.1007/978-981-96-0901-7_4</a> (<span className="font-bold">acceptance rate: 32%</span>)
           </li>
         </ul>
       </div>
@@ -58,7 +58,7 @@ const IndexPage = () => {
             <div className="projectTitle">Ice Planet Mission (ゲーム開発)</div>
             <div>
               制作時期: 2022年<br />
-              Unityを使用して、障害物避けアクションゲームを開発しました。全10ステージで構成される本作は、各ステージをクリアしながら最終面を目指す構造となっています。
+              Unityを使用して、障害物避けアクションゲームを個人開発しました。全10ステージで構成される本作は、各ステージをクリアしながら最終面を目指す構造となっています。
               ステージ内に配置されたコインを集めることでスコアが加算され、ハイスコアへの挑戦も楽しめる要素を実装しました。また、プレイヤーの快適なゲーム体験を重視し、直感的に操作できるUIデザインを心がけました。<br />
               Link: <a href="https://sharemygame.com/@tkbreeze/ice-planet-mission" className="text-blue-600 visited:text-purple-600">https://sharemygame.com/@tkbreeze/ice-planet-mission</a>
             </div>
@@ -69,7 +69,7 @@ const IndexPage = () => {
             <div className="projectTitle">ゲームレビュー投稿サイト</div>
             <div>
               制作時期: 2023年<br />
-              ゲームレビュー投稿サイトを個人開発しました。フレームワークはRuby on Railsを用いており、インフラとしてはAWS EC2サーバーを使用しています。ユーザーがゲームのレビューを投稿し、他のユーザーと共有することができるサイトです。本サイトの特徴は、従来のような単純な評価点だけでなく、ゲームの5つの要素(例: ゲーム性、グラフィック、世界観など)について、良い点・悪い点を評価できる仕組みを実装したことです。これにより、ユーザーは自身が重視する要素に基づいて、より的確にゲーム選びを行えるようになると考えています。<br />
+              ゲームレビュー投稿サイトを個人開発しました。フレームワークはRuby on Railsを用いており、インフラとしてはAWS EC2サーバーを使用しています。ユーザーがゲームのレビューを投稿し、他のユーザーと共有することができるサイトです。本サイトの特長は、単なる総合評価点だけでなく、ゲーム性、グラフィック、世界観などの5つの核となる要素について、それぞれの長所・短所を詳細に評価できるシステムを実装したことです。これにより、ユーザーは自身の嗜好や価値観に基づいてゲームを選択することができ、真に自分に適したゲームとの出会いを実現できると考えています。<br />
               Link: <a href="https://game-review-aggregator.net/" className="text-blue-600 visited:text-purple-600">https://game-review-aggregator.net/</a> GitHub: <GitHubIcon url="https://github.com/tkbreeze/game_review" />
             </div>
           </div>
@@ -85,7 +85,7 @@ const IndexPage = () => {
             <div className="projectTitle">Amazon Connectブログ作成</div>
             <div>
               制作時期: 2023年<br />
-              Alleeks株式会社での長期インターンシップ中に、業務で使用していたAmazon Connectに関する技術情報をまとめたブログを二名で作成しました。私がリーダとなり、タスクスケジュール管理なども行いました。フレームワークとしてはVue.jsを使用し、ORMとしてPrismaを使用しました。マークダウンで記入することで、ブログ記事を簡単に作成できるようにしました。<br />
+              Alleeks株式会社での長期インターンシップ中に、業務で使用していたAmazon Connectに関する技術情報をまとめたブログを二名で作成しました。プロジェクトリーダーとして、タスクの管理やスケジューリングなども担当しました。フレームワークとしてはVue.jsを使用し、ORMとしてPrismaを使用しました。また、マークダウン形式での投稿機能を実装し、ブログ記事を簡単に作成できるようにしました。<br />
               Link: <a href="https://www.alleeks.com/acblog" className="text-blue-600 visited:text-purple-600">https://www.alleeks.com/acblog</a>
             </div>
           </div>
@@ -96,7 +96,7 @@ const IndexPage = () => {
             <div>
               時期: 2023年 - Current<br />
               Motor Imagery Classification(運動想起分類)とは、特定の動作を頭の中でイメージした際に記録された脳波データを基に、どの動作をイメージしていたかを分類するタスクになっています。車椅子の制御や、義手の制御など、様々な応用が期待されています。<br />
-              本研究では、従来の手法では脳波の空間的・時間的特徴料を適切に抽出できていなかったため、コンピュータビジョンの知識を導入することにより、オープンデータセットにおいて既存の手法を上回る精度を達成しました。本研究はACCV2024のポスターに採択されました。現在は、その過程で見つけた課題を解決し、さらなる精度向上を目指しています。<br />
+              従来の手法では脳波の空間的・時間的特徴量を十分に活用できていないという課題がありました。そのため、本研究ではコンピュータビジョン分野の技術を応用することで、それらの特徴を最大限に捉え、「PhysioNet EEG Motor Movement/Imagery Dataset」というオープンデータセットにおいて既存の手法を上回る精度を達成しました。本研究はACCV2024のポスターに採択されました。現在は、その過程で見つけた課題を解決することで、さらなる精度向上を目指しています。<br />
               また、日本電信電話株式会社のR&Dインターンシップでは、脳波計を用いたオンライン学習において学習方法の工夫を行いました。<br />
               Link: <a href="https://doi.org/10.1007/978-981-96-0901-7_4" className="text-blue-600 visited:text-purple-600">https://doi.org/10.1007/978-981-96-0901-7_4</a> <br />
             </div>
